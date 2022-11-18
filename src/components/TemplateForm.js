b1bc8599680150459ccc9df5424d6e7b7fbd6f9d
Skylan Lew <skylanslew@gmail.com>
2022-11-18 15:42:03 -0700

    Add default values, dropdown selector

--- src/components/TemplateForm.js
+++ src/components/TemplateForm.js
@@ -2,33 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function TemplateForm(props){
+  let formTitle = "New Tea";
+  let teaToEdit = [];
+  if(props.editing){
+    teaToEdit = props.tea;
+    formTitle = "Edit" + props.tea.name;
+  }
   return(
     <form onSubmit={props.formSubmissionHandler} className="form">
-      {props.editing ? <h1>Edit {props.tea.name}</h1> : <h1>New Tea</h1>}
+      <h1>{formTitle}</h1>
       <input
         type="text"
         name="name"
-        placeHolder="Tea Name" />
+        placeHolder="Tea Name"
+        defaultValue={teaToEdit.name || ''} />
       <input
         type="text"
         name="origin"
-        placeHolder="Tea Origin" />
+        placeHolder="Tea Origin"
+        defaultValue={teaToEdit.origin || ''} />
       <input
         type="number"
         name="price"
-        step="0.01" />
-      <input
-        type="text"
-        name="color"
-        placeHolder="Tea Color" />
+        step="0.01"
+        defaultValue={teaToEdit.price || ''} />
+      <select name="color" defaultValue={teaToEdit.color || "Red"}>
+        <option value="Black">Black</option>
+        <option value="Green">Green</option>
+        <option value="Red">Red</option>
+        <option value="White">White</option>
+        <option value="Dark">Dark/Fermented</option>
+        <option value="Herbal">Herbal</option>
+        <option value="Oolong">Oolong</option>
+      </select>
       <input
         type="text"
         name="description"
-        placeHolder="Tea Description" />
+        placeHolder="Tea Description"
+        defaultValue={teaToEdit.description || ''} />
       <input 
         type="text"
         name="imageUrl"
-        placeHolder="image URL" />
+        placeHolder="image URL"
+        defaultValue={teaToEdit.imageUrl || ''} />
       <button type="submit">{props.buttonText}</button>
     </form>
   )

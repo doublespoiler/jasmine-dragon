63263a456f42ac1cda2c3feebb6cce22a30414bf
Skylan Lew <skylanslew@gmail.com>
2022-11-18 15:17:32 -0700

    add logo, update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../images/logo.png'
 
 export default function Header(){
   return(
     <header className="header">
-      <img src="" className="header--image" alt="logo" />
+      <img src={logo} className="header--image" alt="logo" />
       <h1 className="header--title">The Jasmine Dragon</h1>
       <h2 className="header--subtitle">Dramatic, Poetic.</h2>
     </header>
